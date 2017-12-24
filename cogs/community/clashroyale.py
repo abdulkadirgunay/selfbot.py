8accef8400566014e41522788cfa12505137bf50
Abdülkadir Günay <30827248+abdulkadirgunay@users.noreply.github.com>
2017-12-24 14:16:55 +0300

    Update clashroyale.py

--- cogs/community/clashroyale.py
+++ cogs/community/clashroyale.py
@@ -41,11 +41,11 @@ class ClashRoyale:
 
     @commands.command()
     async def profile(self, ctx, tag=None):
-        '''Fetch a Clash Royale Profile!'''
+        '''Clash Royale Profilini getirin!'''
         em = discord.Embed(title="Profile")
         em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
         if tag is None and self.tag is None:
-            em.description = "Please add `CR_TAG` to your options. Do `{p}options edit cr_tag <tag>`"
+            em.description = "Lütfen seçeneklerinize `CR_TAG` ekleyin. `{p} seçenekleri değiştirir cr_tag <tag>`"
             return await ctx.send(embed=em)
         elif self.tag is not None:
             tag = self.tag
@@ -54,28 +54,28 @@ class ClashRoyale:
         try:
             profile = await self.client.get_profile(tag)
         except:
-            em.description = "Either API is down or that's an invalid tag."
+            em.description = "API geçersiz veya bu etiket geçersiz."
             return await ctx.send(embed=em)
                                       
         em.title = profile.name
         em.set_thumbnail(url=profile.arena.image_url)
         em.description = f"#{tag}"
         em.url = f"http://cr-api.com/profile/{tag}"
-        em.add_field(name='Current Trophies', value=profile.current_trophies)
-        em.add_field(name='Highest Trophies', value=profile.highest_trophies)
-        em.add_field(name='Legend Trophies', value=f'{profile.legend_trophies}')
-        em.add_field(name='Level', value=profile.level)
-        em.add_field(name='Experience', value=f"{profile.experience[0]}/{profile.experience[1]}")
-        em.add_field(name='Wins/Losses/Draws', value=f'{profile.wins}/{profile.losses}/{profile.draws}')
-        em.add_field(name='Global Rank', value=f'{profile.global_rank}')         
-        em.add_field(name='Clan Info', value=f'{profile.clan_name}\n#{profile.clan_tag}\n{profile.clan_role}')
-        em.add_field(name='Win Streak', value=f'{profile.win_streak}')
-        em.set_footer(text="Powered By cr-api.com", icon_url="http://cr-api.com/static/img/branding/cr-api-logo.png")
+        em.add_field(name='Şu anki Kupalar', value=profile.current_trophies)
+        em.add_field(name='En Yüksek Kupalar', value=profile.highest_trophies)
+        em.add_field(name='Efsane Kupaları', value=f'{profile.legend_trophies}')
+        em.add_field(name='Seviye', value=profile.level)
+        em.add_field(name='Deneyim', value=f"{profile.experience[0]}/{profile.experience[1]}")
+        em.add_field(name='Galibiyetler/Kayıplar/Beraberlikler', value=f'{profile.wins}/{profile.losses}/{profile.draws}')
+        em.add_field(name='Global Sıralama', value=f'{profile.global_rank}')         
+        em.add_field(name='Klan Bilgisi', value=f'{profile.clan_name}\n#{profile.clan_tag}\n{profile.clan_role}')
+        em.add_field(name='Galibiyet Çizgisi', value=f'{profile.win_streak}')
+        em.set_footer(text="CR-api.com tarafından desteklenmektedir.", icon_url="http://cr-api.com/static/img/branding/cr-api-logo.png")
         
         try:
-            em.set_author(name="Profile", icon_url=profile.clan_badge_url)
+            em.set_author(name="Profil", icon_url=profile.clan_badge_url)
         except:
-            em.set_author(name='Profile')
+            em.set_author(name='Profil')
         await ctx.send(embed=em) 
 
 
